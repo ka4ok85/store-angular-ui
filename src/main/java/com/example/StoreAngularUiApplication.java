c2b6c5ac5fa5ce43fcb02c601212723a58cc455f
ka4ok85 <makovetsky.evgeny@gmail.com>
2017-01-23 15:17:16 -0500

    init

---
+++ src/main/java/com/example/StoreAngularUiApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAngularUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAngularUiApplication.class, args);
+	}
+}
